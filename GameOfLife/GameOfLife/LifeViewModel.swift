51494dafe52b1be876ec2145f3cc06c0fb5d40f6
mauriciomaniglia <manigliamauricio@gmail.com>
2025-04-23 08:01:49 -0300

    Create Life view model

---
+++ GameOfLife/GameOfLife/LifeViewModel.swift
@@ -0,0 +1,46 @@
+import SwiftUI
+
+@Observable class LifeViewModel {
+    private(set) var isRunning: Bool
+
+    var lifeModel = LifeModel(cells: 10)
+
+    init() {
+        self.isRunning = false
+    }
+
+    var numberOfCycles: Int {
+        lifeModel.numberOfCycles
+    }
+
+    func reset(gridSize: Int = 10) {
+        self.isRunning = false
+        self.lifeModel = LifeModel(cells: gridSize)
+    }
+
+    func step() {
+        lifeModel.step()
+    }
+
+    func setGridSize(cellNumber: Int) {
+        lifeModel = LifeModel(cells: cellNumber)
+    }
+
+    @MainActor
+    func performNumberOfCycles(number: Int) async {
+        isRunning = true
+
+        for _ in 1...number {
+            let isDifferent = await lifeModel.performStepDelay()
+
+            if !isDifferent || !isRunning { break }
+        }
+
+        isRunning = false
+    }
+
+    @MainActor
+    func stop() async {
+        isRunning = false
+    }
+}
